30dc63b1e3f6bcde40b37fee8bbe8ad8c056104f
rajharsh0205 <rajharsh810201@gmail.com>
2025-02-27 13:09:27 +0530

    [Harsh] Add . Uc6 feature added

--- src/main/java/com/bridgelabz/employeePayrollApp/model/EmployeeModel.java
+++ src/main/java/com/bridgelabz/employeePayrollApp/model/EmployeeModel.java
@@ -2,7 +2,11 @@ package com.bridgelabz.employeePayrollApp.model;
 
 import com.bridgelabz.employeePayrollApp.DTO.EmployeeDTO;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "employee")
 public class EmployeeModel {
@@ -13,33 +17,10 @@ public class EmployeeModel {
     private double salary;
 
 
-    public EmployeeModel( String name, int salary) {
-        this.name = name;
-        this.salary = salary;
+    public EmployeeModel(){}
+    public EmployeeModel( EmployeeDTO employeeDTO) {
+        this.name = employeeDTO.getName();
+        this.salary = employeeDTO.getSalary();
     }
 
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
