1d40dfe961a644205186748d0451c6eae3327cc9
rajharsh0205 <rajharsh810201@gmail.com>
2025-03-02 16:24:03 +0530

    [Harsh] Add . Uc13 feature added

--- src/main/java/com/bridgelabz/employeePayrollApp/model/EmployeeModel.java
+++ src/main/java/com/bridgelabz/employeePayrollApp/model/EmployeeModel.java
@@ -5,27 +5,50 @@ import com.bridgelabz.employeePayrollApp.DTO.EmployeeDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "employee")
-public class EmployeeModel {
+public @Data class EmployeeModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private double salary;
+    private String gender;
+    private String note;
+    private String profilePic;
+    private List<String> department;
 
 
     public EmployeeModel(){}
-    public EmployeeModel( EmployeeDTO employeeDTO) {
-        this.name = employeeDTO.getName();
-        this.salary = employeeDTO.getSalary();
+
+//    public EmployeeModel(int id, EmployeeDTO employeeDTO) {
+//        this.id = id;
+//        this.name = employeeDTO.getName();
+//        this.salary = employeeDTO.getSalary();
+//        this.department = employeeDTO.getDepartment();
+//        this.profilePic = employeeDTO.getProfilePic();
+//        this.note = employeeDTO.getNote();
+//        this.gender = employeeDTO.getGender();
+//    }
+
+    public EmployeeModel(int id, String name, double salary, String gender, String note, String profilePic, List<String> department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+        this.note = note;
+        this.profilePic = profilePic;
+        this.department = department;
     }
+
+
+//    public EmployeeModel(@NotEmpty(message = "Name cannot be empty") @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long") String name, double salary) {
+//    }
 
 }
